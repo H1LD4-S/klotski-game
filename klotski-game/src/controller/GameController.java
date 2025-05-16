e6858c7e631804b0b7aaaf71d388c65d7ef157a9
H1LD4-S <12410416@mail.sustech.edu.cn>
2025-05-16 22:26:37 +0800

    Update GameController.java
    登陆注册和游客

--- klotski-game/src/controller/GameController.java
+++ klotski-game/src/controller/GameController.java
@@ -5,6 +5,8 @@ import model.MapModel;
 import view.game.BoxComponent;
 import view.game.GamePanel;
 
+import java.io.*;
+
 /**
  * It is a bridge to combine GamePanel(view) and MapMatrix(model) in one game.
  * You can design several methods about the game logic in this class.
@@ -12,18 +14,50 @@ import view.game.GamePanel;
 public class GameController {
     private final GamePanel view;
     private final MapModel model;
-
+    private String currentUser;
     public GameController(GamePanel view, MapModel model) {
         this.view = view;
         this.model = model;
         view.setController(this);
     }
+    public void setCurrentUser(String user) {
+        this.currentUser = user;
+    }
+    public void saveGame() {
+        if (currentUser == null) {
+            return; // 游客不能保存
+        }
+        String filePath = currentUser + "_game.data";
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            oos.writeObject(model.getMatrix());
+            oos.writeInt(view.getSteps());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void loadGame() {
+        if (currentUser == null) {
+            return; // 游客不能加载
+        }
+        String filePath = currentUser + "_game.data";
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
+            int[][] savedMatrix = (int[][]) ois.readObject();
+            int savedSteps = ois.readInt();
+            model.setMatrix(savedMatrix);
+            view.clearBoxes();
+            view.initialGame();
+            view.setSteps(savedSteps);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public void restartGame() {
         System.out.println("Do restart game here");
     }
 
-    public boolean doMove(int row, int col, Direction direction){
+    public boolean doMove(int row, int col, Direction direction) {
         if (model.getId(row, col) == 1) {
             int nextRow = row + direction.getRow();
             int nextCol = col + direction.getCol();
@@ -327,54 +361,54 @@ public class GameController {
             if (direction.getCol()==0){
                 if (model.checkInHeightSize(nextRow)) {
                     if (model.getId(nextRow,col)==4){
-                       if (model.checkInHeightSize(nextRow+1)){
-                           if (model.getId(nextRow+direction.getRow(),col)==0){
-                               if (model.checkInWidthSize(col+1)) {
-                                   if (model.getId(row,col+1)==4){
-                                       if (model.getId(nextRow+direction.getRow(),col+1)==0){
-                                           model.getMatrix()[row][col]=0;
-                                           model.getMatrix()[row][col+1]=0;
-                                           model.getMatrix()[nextRow][col]=4;
-                                           model.getMatrix()[nextRow][col+1]=4;
-                                           model.getMatrix()[nextRow+direction.getRow()][col]=4;
-                                           model.getMatrix()[nextRow+direction.getRow()][col+1]=4;
-                                           BoxComponent box = view.getSelectedBox();
-                                           box.setRow(nextRow);
-                                           box.setCol(col);
-                                           box.setLocation(box.getCol() * view.getGRID_SIZE() + 2, box.getRow() * view.getGRID_SIZE() + 2);
-                                           box.repaint();
-                                           return true;
-                                       }else return false;
-                                   }else if (model.getId(nextRow+direction.getRow(),col-1)==0){
-                                       model.getMatrix()[row][col]=0;
-                                       model.getMatrix()[row][col-1]=0;
-                                       model.getMatrix()[nextRow][col]=4;
-                                       model.getMatrix()[nextRow][col-1]=4;
-                                       model.getMatrix()[nextRow+direction.getRow()][col]=4;
-                                       model.getMatrix()[nextRow+direction.getRow()][col-1]=4;
-                                       BoxComponent box = view.getSelectedBox();
-                                       box.setRow(nextRow);
-                                       box.setCol(col);
-                                       box.setLocation(box.getCol() * view.getGRID_SIZE() + 2, box.getRow() * view.getGRID_SIZE() + 2);
-                                       box.repaint();
-                                       return true;
-                                   }else return false;
-                               }else if (model.getId(nextRow+direction.getRow(),col-1)==0){
-                                   model.getMatrix()[row][col]=0;
-                                   model.getMatrix()[row][col-1]=0;
-                                   model.getMatrix()[nextRow][col]=4;
-                                   model.getMatrix()[nextRow][col-1]=4;
-                                   model.getMatrix()[nextRow+direction.getRow()][col]=4;
-                                   model.getMatrix()[nextRow+direction.getRow()][col-1]=4;
-                                   BoxComponent box = view.getSelectedBox();
-                                   box.setRow(nextRow);
-                                   box.setCol(col);
-                                   box.setLocation(box.getCol() * view.getGRID_SIZE() + 2, box.getRow() * view.getGRID_SIZE() + 2);
-                                   box.repaint();
-                                   return true;
-                               }else return false;
-                           }else return false;
-                       }else return false;
+                        if (model.checkInHeightSize(nextRow+1)){
+                            if (model.getId(nextRow+direction.getRow(),col)==0){
+                                if (model.checkInWidthSize(col+1)) {
+                                    if (model.getId(row,col+1)==4){
+                                        if (model.getId(nextRow+direction.getRow(),col+1)==0){
+                                            model.getMatrix()[row][col]=0;
+                                            model.getMatrix()[row][col+1]=0;
+                                            model.getMatrix()[nextRow][col]=4;
+                                            model.getMatrix()[nextRow][col+1]=4;
+                                            model.getMatrix()[nextRow+direction.getRow()][col]=4;
+                                            model.getMatrix()[nextRow+direction.getRow()][col+1]=4;
+                                            BoxComponent box = view.getSelectedBox();
+                                            box.setRow(nextRow);
+                                            box.setCol(col);
+                                            box.setLocation(box.getCol() * view.getGRID_SIZE() + 2, box.getRow() * view.getGRID_SIZE() + 2);
+                                            box.repaint();
+                                            return true;
+                                        }else return false;
+                                    }else if (model.getId(nextRow+direction.getRow(),col-1)==0){
+                                        model.getMatrix()[row][col]=0;
+                                        model.getMatrix()[row][col-1]=0;
+                                        model.getMatrix()[nextRow][col]=4;
+                                        model.getMatrix()[nextRow][col-1]=4;
+                                        model.getMatrix()[nextRow+direction.getRow()][col]=4;
+                                        model.getMatrix()[nextRow+direction.getRow()][col-1]=4;
+                                        BoxComponent box = view.getSelectedBox();
+                                        box.setRow(nextRow);
+                                        box.setCol(col);
+                                        box.setLocation(box.getCol() * view.getGRID_SIZE() + 2, box.getRow() * view.getGRID_SIZE() + 2);
+                                        box.repaint();
+                                        return true;
+                                    }else return false;
+                                }else if (model.getId(nextRow+direction.getRow(),col-1)==0){
+                                    model.getMatrix()[row][col]=0;
+                                    model.getMatrix()[row][col-1]=0;
+                                    model.getMatrix()[nextRow][col]=4;
+                                    model.getMatrix()[nextRow][col-1]=4;
+                                    model.getMatrix()[nextRow+direction.getRow()][col]=4;
+                                    model.getMatrix()[nextRow+direction.getRow()][col-1]=4;
+                                    BoxComponent box = view.getSelectedBox();
+                                    box.setRow(nextRow);
+                                    box.setCol(col);
+                                    box.setLocation(box.getCol() * view.getGRID_SIZE() + 2, box.getRow() * view.getGRID_SIZE() + 2);
+                                    box.repaint();
+                                    return true;
+                                }else return false;
+                            }else return false;
+                        }else return false;
                     }else if (model.getId(nextRow,col)==0){
                         if (model.checkInWidthSize(col+1)){
                             if (model.getId(row,col+1)==4){
@@ -523,8 +557,8 @@ public class GameController {
             }
         }
         return false;
-
     }
+
     //todo: add other methods such as loadGame, saveGame...
 
 }
