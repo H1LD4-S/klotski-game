51dc9882fad2fe1ae62b961d0d3433c62e64a6a2
Ez921 <993038598@qq.com>
2025-05-16 10:41:22 +0800

    Update Main.java

--- klotski-game/src/Main.java
+++ klotski-game/src/Main.java
@@ -10,10 +10,10 @@ public class Main {
             LoginFrame loginFrame = new LoginFrame(280, 280);
             loginFrame.setVisible(true);
             MapModel mapModel = new MapModel(new int[][]{
-                    {1, 2, 2, 1},
-                    {1, 3, 2, 2},
-                    {1, 3, 4, 4},
-                    {0, 0, 4, 4}
+                    {2, 2, 2, 2, 1},
+                    {4, 4, 3, 1, 0},
+                    {4, 4, 3, 1, 0},
+                    {2, 2, 2, 2，1}
             });
             GameFrame gameFrame = new GameFrame(600, 450, mapModel);
             gameFrame.setVisible(false);
