3acfbba470036ee1305e85b21a76ef1ab430b7c3
Ez921 <993038598@qq.com>
2025-05-17 01:44:48 +0800

    Update GameFrame.java

--- klotski-game/src/view/game/GameFrame.java
+++ klotski-game/src/view/game/GameFrame.java
@@ -18,18 +18,20 @@ public class GameFrame extends JFrame {
     private GamePanel gamePanel;
 
     public GameFrame(int width, int height, MapModel mapModel) {
-        this.setTitle("2025 CS109 Project Demo");
+        this.setTitle("华容道");
         this.setLayout(null);
         this.setSize(width, height);
         gamePanel = new GamePanel(mapModel);
-        gamePanel.setLocation(30, height / 2 - gamePanel.getHeight() / 2);
+        gamePanel.setLocation(30, 30);
         this.add(gamePanel);
         this.controller = new GameController(gamePanel, mapModel);
 
-        this.restartBtn = FrameUtil.createButton(this, "Restart", new Point(gamePanel.getWidth() + 80, 120), 80, 50);
-        this.loadBtn = FrameUtil.createButton(this, "Load", new Point(gamePanel.getWidth() + 80, 210), 80, 50);
-        this.stepLabel = FrameUtil.createJLabel(this, "Start", new Font("serif", Font.ITALIC, 22), new Point(gamePanel.getWidth() + 80, 70), 180, 50);
+        this.restartBtn = FrameUtil.createButton(this, "重新开始", new Point(gamePanel.getWidth() + 180, 620), 100, 50);
+        this.loadBtn = FrameUtil.createButton(this, "载入", new Point(gamePanel.getWidth() + 180, 710), 80, 50);
+        this.stepLabel = FrameUtil.createJLabel(this, "开始游戏", new Font("serif", Font.ITALIC, 35), new Point(gamePanel.getWidth() + 180, 470), 180, 50);
         gamePanel.setStepLabel(stepLabel);
+
+        
         this.loadBtn.addActionListener(e -> {
             if (currentUser != null) {
                 controller.saveGame();
