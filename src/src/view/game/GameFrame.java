e7386e1deaa82adaad01fc29d6e49d1e39e42b86
H1LD4-S <12410416@mail.sustech.edu.cn>
2025-05-22 03:03:14 +0800

    Update GameFrame.java

--- src/src/view/game/GameFrame.java
+++ src/src/view/game/GameFrame.java
@@ -7,6 +7,9 @@ import view.login.BackgroundPanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
 
 public class GameFrame extends JFrame {
 
@@ -68,6 +71,29 @@ public class GameFrame extends JFrame {
         //todo: add other button here
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowActivated(WindowEvent e) {
+                if (!controller.isInitialized()) {
+                    String filePath = controller.getCurrentUser() + "_level" + controller.getCurrentLevel() + ".data";
+                    File saveFile = new File(filePath);
+
+                    if (saveFile.exists()) {
+                        controller.loadGame();
+                    } else {
+                        controller.restartGame();
+                    }
+                    controller.markInitialized();
+                }// 由于打开第二关和第三关都会和第一关一样，而点击重启会正常，因此首次激活窗口时强制初始化
+                //woc但是存档功能又没了！！啊啊啊啊啊啊啊啊
+                //所以！应该写成只有在没有存档时才会初始化！！
+                //已修改，欧耶
+                //不行，又失败了woc，/(ㄒoㄒ)/
+                //试试“强制深拷贝”好了
+                //不对，没有用，根本不是这里的问题
+            }
+        });
     }
     public void setUser(String user) {
         this.currentUser = user;
@@ -80,6 +106,9 @@ public class GameFrame extends JFrame {
     }
     public void setCurrentLevel(int level){
         this.currentLevel=level;
+        controller.setCurrentLevel(level);//原来问题在这里！！！！！终于找到了哈哈哈哈哈哈哈哈哈哈
     }
 
+
 }
+
